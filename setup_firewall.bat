a87e4893c468212f8772b558deea7f23f6c0c639
si ck <oakgamingth23@gmail.com>
2026-06-04 18:10:52 +0700

    Update rain player and controls

--- setup_firewall.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Adding firewall rules for more-focus...
-netsh advfirewall firewall delete rule name="MoreFocus HTTP" >nul 2>&1
-netsh advfirewall firewall delete rule name="MoreFocus 8080" >nul 2>&1
-netsh advfirewall firewall add rule name="MoreFocus HTTP"  dir=in action=allow protocol=TCP localport=80
-netsh advfirewall firewall add rule name="MoreFocus 8080" dir=in action=allow protocol=TCP localport=8080
-echo Done.
-netsh advfirewall firewall show rule name="MoreFocus HTTP"
-pause
